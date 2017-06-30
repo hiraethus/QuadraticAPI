a95952e64e446a78d6c7f36511a0b885e9f5b0c9
Mike Clack Jones <mjclackjones@gmail.com>
2017-06-30 13:54:33 +0100

    * Refactor out creating result data frame into separate function * Refactor out deleting sig on server to separate function

--- R/main.R
+++ R/main.R
@@ -1,8 +1,8 @@
 analyze <- function(query.gene.sig,
                     analysis.set='Drug_Name',
                     num.rand.sigs=2000,
-                    endpoint=getOption('quadratic.endpoint', default = 'http://127.0.0.1:8090'))
-{
+                    endpoint=getOption('quadratic.endpoint', default = 'http://127.0.0.1:8090')) {
+
   .StopIfQuadraticEndpointDown(endpoint)
 
   sig.id <- .GenerateRandomSigID()
@@ -11,30 +11,10 @@ analyze <- function(query.gene.sig,
   job.id.timestamped <- .SubmitJob(sig.id, analysis.set, num.rand.sigs, endpoint)
 
   .WaitForJobToFinish(job.id.timestamped, endpoint)
+  .DeleteQueryGeneSignature(sig.id, endpoint)
 
   result <- .RetrieveResult(job.id.timestamped, endpoint)
-
-  num.results <- length(result$resultList)
-  results.df <- data.frame(id=character(num.results),
-                           z.score=numeric(num.results),
-                            connection.score=numeric(num.results),
-                            p.val=numeric(num.results),
-                            num.profiles=numeric(num.results),
-                            stringsAsFactors = FALSE)
-
-  for (i in seq(num.results)) {
-    results.df[i, 'id']                   <-  as.character(result$resultList[[i]][['id']])
-    results.df[i, 'z.score']     <-  result$resultList[[i]][['cs']]
-    results.df[i, 'connection.score'] <-  result$resultList[[i]][['rawCs']]
-    results.df[i, 'p.val']                <-  result$resultList[[i]][['pVal']]
-    results.df[i, 'num.profiles']         <-  result$resultList[[i]][['n']]
-  }
-
-  # clean up - delete sig
-  sig.id.endpoint <- paste0('/api/sigs/', sig.id)
-  httr::DELETE(paste0(endpoint, sig.id.endpoint))
-
-  results.df
+  .ResultToDataFrame(result)
 }
 
 .DataFrameToQGS <- function(qgs.df) {
@@ -55,6 +35,11 @@ analyze <- function(query.gene.sig,
 
 .SubmitQueryGeneSignature <- function(sig.id, probes, endpoint) {
   httr::POST(paste0(endpoint, '/api/sigs'), body=list(id=sig.id, probes=as.list(probes)), encode='json')
+}
+
+.DeleteQueryGeneSignature <- function(sig.id, endpoint) {
+  sig.id.endpoint <- paste0('/api/sigs/', sig.id)
+  httr::DELETE(paste0(endpoint, sig.id.endpoint))
 }
 
 .SubmitJob <- function(sig.id, analysis.set, num.rand.sigs, endpoint) {
@@ -99,6 +84,26 @@ analyze <- function(query.gene.sig,
   }
 
   httr::content(result.response)
+}
+
+.ResultToDataFrame <- function(result) {
+  num.results <- length(result$resultList)
+  results.df <- data.frame(id=character(num.results),
+                           z.score=numeric(num.results),
+                           connection.score=numeric(num.results),
+                           p.val=numeric(num.results),
+                           num.profiles=numeric(num.results),
+                           stringsAsFactors = FALSE)
+
+  for (i in seq(num.results)) {
+    results.df[i, 'id']                   <-  as.character(result$resultList[[i]][['id']])
+    results.df[i, 'z.score']     <-  result$resultList[[i]][['cs']]
+    results.df[i, 'connection.score'] <-  result$resultList[[i]][['rawCs']]
+    results.df[i, 'p.val']                <-  result$resultList[[i]][['pVal']]
+    results.df[i, 'num.profiles']         <-  result$resultList[[i]][['n']]
+  }
+
+  results.df
 }
 
 .StopIfQuadraticEndpointDown <- function(endpoint) {
